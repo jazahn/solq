981e0c3136f232cca00f9eaa5e7c3a651ded221c
JaZahn <jcleveng@fas.harvard.edu>
2015-03-30 11:07:12 -0400

    forcing the load to happen as soon as the page loads

--- js/app/main.js
+++ js/app/main.js
@@ -8,37 +8,37 @@ require(["jquery"], function($) {
         var playhead = document.getElementById('playhead'); // playhead
 
         var timeline = document.getElementById('timeline'); // timeline
-// timeline width adjusted for playhead
+        // timeline width adjusted for playhead
         var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
 
-// timeupdate event listener
+        // timeupdate event listener
         music.addEventListener("timeupdate", timeUpdate, false);
 
-//Makes timeline clickable
+        //Makes timeline clickable
         timeline.addEventListener("click", function (event) {
             moveplayhead(event);
             music.currentTime = duration * clickPercent(event);
         }, false);
 
-// returns click as decimal (.77) of the total timelineWidth
+        // returns click as decimal (.77) of the total timelineWidth
         function clickPercent(e) {
             return (e.pageX - timeline.offsetLeft) / timelineWidth;
         }
 
-// Makes playhead draggable
+        // Makes playhead draggable
         playhead.addEventListener('mousedown', mouseDown, false);
         window.addEventListener('mouseup', mouseUp, false);
 
-// Boolean value so that mouse is moved on mouseUp only when the playhead is released
+        // Boolean value so that mouse is moved on mouseUp only when the playhead is released
         var onplayhead = false;
-// mouseDown EventListener
+        // mouseDown EventListener
         function mouseDown() {
             onplayhead = true;
             window.addEventListener('mousemove', moveplayhead, true);
             music.removeEventListener('timeupdate', timeUpdate, false);
         }
-// mouseUp EventListener
-// getting input from all mouse clicks
+        // mouseUp EventListener
+        // getting input from all mouse clicks
         function mouseUp(e) {
             if (onplayhead == true) {
                 moveplayhead(e);
@@ -49,8 +49,8 @@ require(["jquery"], function($) {
             }
             onplayhead = false;
         }
-// mousemove EventListener
-// Moves playhead as user drags
+        // mousemove EventListener
+        // Moves playhead as user drags
         function moveplayhead(e) {
             var newMargLeft = e.pageX - timeline.offsetLeft;
             if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
@@ -64,18 +64,21 @@ require(["jquery"], function($) {
             }
         }
 
-// timeUpdate
-// Synchronizes playhead position with current point in audio
+        // timeUpdate
+        // Synchronizes playhead position with current point in audio
         function timeUpdate() {
+            //console.log("timeUpdate");
             var playPercent = timelineWidth * (music.currentTime / duration);
             playhead.style.marginLeft = playPercent + "px";
+            console.log(music.currentTime);
+            console.log(duration);
             if (music.currentTime == duration) {
                 pButton.className = "";
                 pButton.className = "play";
             }
         }
 
-//Play and Pause
+        //Play and Pause
         function play() {
             // start music
             if (music.paused) {
@@ -96,7 +99,8 @@ require(["jquery"], function($) {
             duration = music.duration;
         }, false);
 
-
+        // forcing load so we can get the duration
+        music.load();
         $('#pButton').click(play);
 
     });
