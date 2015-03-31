fa6615a69a5b8f12037d05a724c00a52d79f7e5c
JaZahn <jcleveng@fas.harvard.edu>
2015-03-31 10:42:00 -0400

    adding a record button

--- js/app/main.js
+++ js/app/main.js
@@ -3,11 +3,14 @@ require(["jquery", "Player"], function($, Player) {
         var initial_player_config = {
             music: document.getElementById('music'),
             playButton: document.getElementById('pButton'),
+            recordButton: document.getElementById('rButton'),
             playhead: document.getElementById('playhead'),
             timeline: document.getElementById('timeline')
         };
-
         var initialPlayer = new Player(initial_player_config);
+
+
+
 
     });
 
